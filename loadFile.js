c1a777a238b6cd45e923fdd6fc1fac28d60974de
Esteban Mata <emata@akurey.com>
2022-10-12 08:33:00 -0600

    Using rxjs instead of Web Workers, problem with observables

--- loadFile.js
+++ loadFile.js
@@ -1,31 +1,14 @@
-import * as THREE from "three";
-import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js";
-import { LASLoader } from "@loaders.gl/las";
-import { load } from "@loaders.gl/core";
-
 onmessage = async function (message) {
-  // File data
-  const data = await load(message.data.name, LASLoader);
+  let headers = new Headers();
+  const fileNumber = message.data.fileNumber;
+  headers.append("Content-Type", "application/json");
+  headers.append("Accept", "application/json");
+  headers.append("Origin", "http://localhost:3000");
 
-  let geos = [];
+  const response = await fetch("http://localhost:3000/puntos/" + fileNumber, {
+    method: "GET",
+    headers: headers,
+  }).then((response) => response.json());
 
-  for (
-    let i = 0;
-    i < data.attributes.POSITION.value.length;
-    i += data.attributes.POSITION.size
-  ) {
-    const x = data.attributes.POSITION.value[0 + i],
-      y = data.attributes.POSITION.value[1 + i],
-      z = data.attributes.POSITION.value[2 + i];
-
-    const geometry = new THREE.SphereBufferGeometry(1, 8, 8);
-    geometry.translate(x, y, z);
-
-    geos.push(geometry);
-  }
-
-  const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geos, false);
-  // debugger;
-
-  this.postMessage(mergedGeometry.toJSON());
+  this.postMessage(response);
 };
--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 import * as THREE from "three";
-import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js";
 
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { BufferGeometryLoader, PerspectiveCamera, Vector3 } from "three";
+
+import {
+  range,
+  asapScheduler,
+  Observable,
+  interval,
+  tap,
+  share,
+  asyncScheduler,
+} from "rxjs";
+
+import {
+  filter,
+  observeOn,
+  retry,
+  take,
+  buffer,
+  skipWhile,
+  skip,
+  takeWhile,
+} from "rxjs/operators";
 
 let scene, camera, renderer, controls;
 let objects = [];
 
 const geoLoader = new BufferGeometryLoader();
-let filesLoaded = 1;
-var maxWorkers = navigator.hardwareConcurrency || 4;
-
-// const workers = [
-//   new Worker("loadFile.js", { type: "module" }),
-//   new Worker("loadFile.js", { type: "module" }),
-//   new Worker("loadFile.js", { type: "module" }),
-//   new Worker("loadFile.js", { type: "module" }),
-//   new Worker("loadFile.js", { type: "module" }),
-// ];
+let filesLoaded = 0;
+let maxFiles = 0;
+await fetch("http://localhost:3000/puntos")
+  .then((response) => response.json())
+  .then((data) => {
+    maxFiles = data.length;
+  });
 
 const element = document.getElementById("canvas");
 
@@ -46,51 +63,91 @@ function render() {
   requestAnimationFrame(render);
 }
 
-function loadModel(folderName) {
-  if (filesLoaded <= 6) {
-    let worker = new Worker("loadFile.js", { type: "module" });
-    worker.onmessage = onWorkerDone;
-    worker.postMessage({
-      name: folderName + "/" + folderName + "_" + filesLoaded++ + ".las",
+// let observer = new Observable(function subscribe(subscriber) {
+//   while (filesLoaded < maxFiles) {
+//     subscriber.next(filesLoaded++);
+//   }
+//   subscriber.complete();
+// });
+
+async function getData(fileNumber) {
+  let headers = new Headers();
+
+  headers.append("Content-Type", "application/json");
+  headers.append("Accept", "application/json");
+  headers.append("Origin", "http://localhost:3000");
+
+  const response = await fetch("http://localhost:3000/puntos/" + fileNumber, {
+    method: "GET",
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      let object;
+      let mergedGeometry = geoLoader.parse(response);
+
+      for (let obj of objects) {
+        if (obj.name === "puntos") {
+          object = obj;
+        }
+      }
+
+      if (object === undefined) {
+        object = new THREE.Object3D();
+        objects.push(object);
+        object.name = "puntos";
+        scene.add(object);
+      }
+
+      let mat = new THREE.PointsMaterial({
+        size: 0.7,
+        vertexColors: true,
+        transparent: true,
+      });
+
+      let chunk = new THREE.Points(mergedGeometry, mat);
+
+      // GeometryCompressionUtils.compressPositions(chunk);
+      // GeometryCompressionUtils.compressNormals(chunk, "ANGLES");
+      // GeometryCompressionUtils.compressUvs(chunk);
+
+      object.add(chunk);
+      object.updateMatrix();
+
+      let boundingBox = new THREE.Box3().setFromObject(object);
+      const vec = new Vector3();
+      boundingBox.getCenter(vec);
+
+      camera.position.x = vec.x;
+      camera.position.y = vec.y;
+      camera.position.z = vec.z + 300;
+    });
+}
+
+function generateObservables(amountOfObservables) {
+  let amountPerObservable = maxFiles / amountOfObservables;
+
+  for (let i = 0; i < amountOfObservables; i++) {
+    let observer = interval(0.01).pipe(
+      tap((x) => console.log(i + " is processing: " + x)),
+
+      skip(filesLoaded),
+      takeWhile(
+        (val) => val < filesLoaded + amountPerObservable && val < maxFiles,
+        false
+      ),
+      observeOn(asyncScheduler)
+    );
+
+    // let observer = range(filesLoaded, filesLoaded + amountPerObservable).pipe(
+    //   observeOn(asyncScheduler)
+    // );
+    filesLoaded += amountPerObservable;
+    observer.subscribe({
+      next: (v) => getData(v),
+      error: (e) => console.error(e),
+      complete: () => console.info(i + " finished"),
     });
   }
 }
-
-function onWorkerDone(message) {
-  let object;
-
-  const mergedGeometry = geoLoader.parse(message.data);
-
-  for (const obj of objects) {
-    if (obj.name === "puntos") {
-      object = obj;
-    }
-  }
-
-  if (object === undefined) {
-    object = new THREE.Object3D();
-    objects.push(object);
-    object.name = "puntos";
-    scene.add(object);
-  }
-
-  let chunk = new THREE.Mesh(
-    mergedGeometry,
-    new THREE.MeshBasicMaterial({ color: "#ffffff" })
-  );
-
-  object.add(chunk);
-  object.updateMatrix();
-
-  let boundingBox = new THREE.Box3().setFromObject(object);
-  const vec = new Vector3();
-  boundingBox.getCenter(vec);
-
-  camera.position.x = vec.x;
-  camera.position.y = vec.y;
-  camera.position.z = vec.z + 50;
-
-  this.terminate();
-}
-
-loadModel("puntos");
+generateObservables(4);
